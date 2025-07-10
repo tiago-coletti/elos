3b2a4cf26f16b03432603ac3789aa9b3db50861e
Tiago Alfonso Neumann Coletti <tiagoalfonsoneumann123@gmail.com>
2025-07-10 09:31:53 -0300

    Adiciona pastas vazias com .gitkeep

--- Elos/src/main/java/model/entity/Resposta.java
+++ Elos/src/main/java/model/entity/Resposta.java
@@ -29,42 +29,55 @@ public class Resposta {
 	public int getId() {
 		return id;
 	}
+	
 	public void setId(int id) {
 		this.id = id;
 	}
+	
 	public int getResposta() {
 		return resposta;
 	}
+	
 	public void setResposta(int resposta) {
 		this.resposta = resposta;
 	}
+	
 	public int getAvaliacaoId() {
 		return avaliacaoId;
 	}
+	
 	public void setAvaliacaoId(int avaliacaoId) {
 		this.avaliacaoId = avaliacaoId;
 	}
+	
 	public int getPerguntaId() {
 		return perguntaId;
 	}
+	
 	public void setPerguntaId(int perguntaId) {
 		this.perguntaId = perguntaId;
 	}
+	
 	public String getCreated_at() {
 		return created_at;
 	}
+	
 	public void setCreated_at(String created_at) {
 		this.created_at = created_at;
 	}
+	
 	public String getUpdated_at() {
 		return updated_at;
 	}
+	
 	public void setUpdated_at(String updated_at) {
 		this.updated_at = updated_at;
 	}
+	
 	public String getDeleted_at() {
 		return deleted_at;
 	}
+	
 	public void setDeleted_at(String deleted_at) {
 		this.deleted_at = deleted_at;
 	}
