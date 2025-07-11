687653ee2f6ce9bebf36ea6c1a17b16438577d84
Tiago Alfonso Neumann Coletti <71907679+tiago-coletti@users.noreply.github.com>
2025-07-11 11:22:46 -0300

    Adição de Filter para autenticação de usuários

--- Elos/src/main/java/controller/empreendimento/LoginServlet.java
+++ Elos/src/main/java/controller/empreendimento/LoginServlet.java
@@ -1,4 +1,4 @@
-package controller;
+package controller.empreendimento;
 
 import jakarta.servlet.RequestDispatcher;
 import jakarta.servlet.ServletException;
---
+++ Elos/src/main/java/filters/AuthEmpreendimentoFilter.java
@@ -0,0 +1,47 @@
+package filters;
+
+import jakarta.servlet.Filter;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.FilterConfig;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.ServletRequest;
+import jakarta.servlet.ServletResponse;
+import jakarta.servlet.annotation.WebFilter;
+import jakarta.servlet.http.HttpFilter;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpSession;
+
+import java.io.IOException;
+
+
+@WebFilter(urlPatterns = {})
+public class AuthEmpreendimentoFilter extends HttpFilter implements Filter {
+
+	public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) 
+			throws IOException, ServletException {
+		
+        HttpServletRequest req = (HttpServletRequest) request;
+        HttpServletResponse res = (HttpServletResponse) response;
+
+        HttpSession session = req.getSession(false); 
+        
+        // Verifica se está logado (como empreendimento)
+        if (session == null || session.getAttribute("idEmpreendimento") == null) {
+            res.sendRedirect(req.getContextPath() + "/index.html?error=4");
+            return;
+        }
+
+		// Segue com a requisição
+		chain.doFilter(request, response);
+	}
+
+	public void init(FilterConfig fConfig) throws ServletException {
+		// TODO Auto-generated method stub
+	}
+	
+	public void destroy() {
+		// TODO Auto-generated method stub
+	}
+
+}
